429b5df2b7b8fe71a5643a80c0729864e0a72712
hangeunhyeong <hgh7546@naver.com>
2024-05-01 11:50:12 +0900

    lv1 6번

--- App.java
+++ App.java
@@ -1,70 +1,74 @@
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
-    public class App {
+public class App {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-            int firstInt;
-            int secondInt;
-            char operator;
-            double result=0;
-            String endCommand="exit";
-            String decideEnd;
+        LinkedList<Double> resultList = new LinkedList<>();
+        int lastIndex = 0;
 
-            do{
-                System.out.println("첫 번째 숫자를 입력하세요: ");
-                firstInt=sc.nextInt();
-                System.out.println("두 번째 숫자를 입력하세요: ");
-                secondInt=sc.nextInt();
+        int firstInt;
+        int secondInt;
+        char operator;
+        double result = 0;
+        String endCommand = "exit";
+        String decideEnd;
 
-                System.out.println("사칙연산 기호를 입력하세요: ");
-                operator=sc.next().charAt(0);
+        do {
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            firstInt = sc.nextInt();
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            secondInt = sc.nextInt();
 
-                switch (operator){
-                    case '+':
-                        result = firstInt+secondInt;
-                        break;
-                    case '-':
-                        result = firstInt-secondInt;
-                        break;
-                    case '*':
-                        result = firstInt*secondInt;
-                        break;
-                    case '/':
-                    {
-                        if (secondInt == 0)
-                            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            operator = sc.next().charAt(0);
 
-                        else
-                            result = (double) firstInt / secondInt;
-                    }
+            switch (operator) {
+                case '+':
+                    result = firstInt + secondInt;
                     break;
+                case '-':
+                    result = firstInt - secondInt;
+                    break;
+                case '*':
+                    result = firstInt * secondInt;
+                    break;
+                case '/': {
+                    if (secondInt == 0)
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
+
+                    else
+                        result = (double) firstInt / secondInt;
                 }
+                break;
+            }
 
-                System.out.println("결과: "+ result);
-                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-                String buffer;
-                buffer=sc.nextLine();
-                decideEnd= sc.nextLine();
+            System.out.println("결과: " + result);
+            resultList.add(result);
+            lastIndex++;
 
-
-            }while(!decideEnd.equals(endCommand) );
-
-
-
+            if (lastIndex > 10) {
+                resultList.remove(0);
+            }
 
 
-        }
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String buffer;
+            buffer = sc.nextLine();
+            decideEnd = sc.nextLine();
 
 
+        } while (!decideEnd.equals(endCommand));
 
 
     }
 
 
+}
 
 
